d0fbe6dd61e46282b537e27024f7dad2e625ab4f
CroseaMors <143801605+CroseaMors@users.noreply.github.com>
2025-03-09 17:21:06 +0800

    Started to make footer
    still in progress

---
+++ FRONTEND/src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './footer.css'; 
+
+export default function Navigation() {
+  console.log("footer component rendered!");
+    return (
+        <div className='footer'></div>
+    );
+}
