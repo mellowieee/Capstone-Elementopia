99eed268ded25c87d4743ff09022d0dc34fefcb4
Dollano, Jericho Sam <tikodollano@gmail.com>
2025-04-21 21:19:49 +0800

    WIP: DTOs and filter updates

--- BACKEND/src/main/java/com/elementopia/database/repository/AchievementTabRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elementopia.database.repository;
-
-import com.elementopia.database.entity.AchievementTabEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AchievementTabRepository extends JpaRepository<AchievementTabEntity, AchievementTabEntity.AchievementTabEntityId> {
-}
